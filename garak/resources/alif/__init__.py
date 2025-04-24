5ce20405a5245d429473d0036796268d5721c7a8
Erick Galinkin <egalinkin@nvidia.com>
2025-04-24 09:51:11 -0400

    Initial code for Alif

---
+++ garak/resources/alif/__init__.py
@@ -0,0 +1,7 @@
+# resources/alif/__init__.py
+
+# SPDX-FileCopyrightText: Portions Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# ALIF implementation derived from https://github.com/TASER2023/TASER/ under Apache-2.0 license.
+# Paper can be found at: https://arxiv.org/abs/2408.01808
